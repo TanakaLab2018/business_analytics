981df64f2a2405ab33df2071fbf6d953b3a8b8d1
test <aaa@bbb.c>
2018-04-23 18:53:39 +0900

    edit 6_5

--- sample_code/6/6_5.py
+++ sample_code/6/6_5.py
@@ -1,4 +1,5 @@
 from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x, a, b, c):
@@ -8,3 +9,7 @@ xdata = np.linspace(-10, 10, 20)
 ydata = f(xdata, 1, 1, 1) + np.random.normal(size=len(xdata))
 param, cov = curve_fit(f, xdata, ydata)
 print(param)
+
+plt.plot(xdata, f(xdata, param[0], param[1], param[2]))
+plt.plot(xdata, ydata, 'o')
+plt.show()
