211aec0f6a922620f96056d9660151af9a629aa4
test <aaa@bbb.c>
2018-04-23 19:27:33 +0900

    create problem_2

---
+++ problem_code/problem_2.py
@@ -0,0 +1,11 @@
+from scipy.optimize import minimize
+import numpy as np
+
+def f(x):
+    c = np.array([0, 1.5, 2.25, 2.625])
+    return np.sum([(c[i] - x[0]*(1. - (x[1])**i))**2  for i in range(1,4)])
+
+x0 = [1.,0.8]
+res = minimize(f, x0, method='Nelder-Mead')
+print(res.x)
+print(res)
