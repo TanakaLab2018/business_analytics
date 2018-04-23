b027e58ec8d6de11072a829c723f2621784079b5
test <aaa@bbb.c>
2018-04-23 20:30:58 +0900

    edit problem_1.md

--- problem_code/problem_1.py
+++ problem_code/problem_1.py
@@ -1,6 +1,6 @@
 from scipy.optimize import minimize, rosen, rosen_der, rosen_hess
 
-x0 = [-2.0*i for i in range(-1,10,1)]
+x0 = [2.0*i for i in range(-1,10,1)]    #x_0=(-2.0,-1.8,\cdots,1.8,2.0)
 print('initial vector:\n',x0)
 
 res = minimize(rosen, x0, method='Nelder-Mead')
