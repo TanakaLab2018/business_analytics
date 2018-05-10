0afcfb4d7e9c07427f2c269fabc69b3d03f13344
Ryoi <aaa@bbb.c>
2018-05-10 23:36:58 +0900

    create 8

---
+++ sample_code/8/8_01.py
@@ -0,0 +1,14 @@
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
+
+n = 20
+x1 = np.linspace(1, n-1, n)
+y = 3.14*x1 + le-6 * np.random.random(n)
+
+plt.plot(x1, y, '.')
+plt.xlabel('x1')
+plt.ylabel('y')
+plt.xlim((o, x1.max()+1))
+
+plt.show()
