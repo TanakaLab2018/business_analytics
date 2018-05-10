bb52a3e6f24dbfa72e0cff52608ca1d94e587421
Ryoi <aaa@bbb.c>
2018-05-10 22:08:10 +0900

    edit SDOS

---
+++ sample_code/7/7_01.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+s = pd.Series(['Alice', 'Bob'])
+s = pd.Series({0:'Alice',1:'Bob'})
+
+print(s)
---
+++ sample_code/7/7_02.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+d = pd.DataFrame([('Alice',20),('Bob',24)], columns=['Name', 'Age'])
+d = pd.DataFrame({'Name':['Alice','Bob'],'Age':[20,24]})
+
+print(d)
