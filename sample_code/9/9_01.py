0c7788b8dd8d897e6d25244e17fc67e74ffab114
Ryoi <aaa@bbb.c>
2018-05-12 18:06:39 +0900

    create 01

---
+++ sample_code/9/9_01.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy.linalg as linalg
+from sklearn.datasets import *
+import matplotlib.pyplot as plt
+import sklearn.svm as svm
+
+print("Hello scikit-learn")
+
+np.random.seed(0)
+X, Y = make_classification(n_features=2, n_redundant=0, n_informative=2)
+iy = (Y==1)
+iny = (Y==0)
+plt.scatter(X[iy, 0], X[iy, 1], marker='o')
+plt.scatter(X[iny, 0], X[iny, 1], marker='x')
+plt.show()
+plt.close()
+
+clf = svm.SVC(kernel='linear', C=1.0)
+clf.fit(X, Y)
+
+xx, yy = np.meshgrid(np.linspace(-3, 3, 500), np.linspace(-3, 3, 500))
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])    #ravelのあとに()が必要
+Z = Z.reshape(xx.shape)
+
+ctr = plt.contour(xx, yy, Z, levels=[0], linetypes='--')
+plt.scatter(X[iy, 0], X[iy, 1], marker='o')
+plt.scatter(X[iny, 0], X[iny, 1], marker='x')
+plt.axis([xx.min(), xx.max(), yy.min(), yy.max()])
+
+plt.show()
