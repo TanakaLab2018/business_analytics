a0381f3f9be60be56dd171bae94b933e33297827
Ryoi_laptop <ryoi084@outlook.jp>
2018-05-23 17:51:35 +0900

    edit 04

--- sample_code/9/9_04.py
+++ sample_code/9/9_04.py
@@ -2,26 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.svm as svm
 
-np.random.seed(100)
-n = 50
+np.random.seed(1000)
+n = 1000
 x = np.sort(np.random.uniform(-np.pi,np.pi,n))
-y = np.sin(x) + np.random.randn(n)*0.3
+y = np.sin(x) + np.random.randn(n)*0.2
 
 svr_rbf = svm.SVR(kernel='rbf', C=1000, gamma=0.1)
-svr_lin = svm.SVR(kernel='linear', C=1000)
-svr_poly = svm.SVR(kernel='poly', C=1000, degree=3)
 
 x = x.reshape(-1,1)
 y = y.reshape(-1,1)
 
 y_rbf = svr_rbf.fit(x, y).predict(x)
-y_lin = svr_lin.fit(x, y).predict(x)
-y_poly = svr_poly.fit(x, y).predict(x)
 
-plt.scatter(x, y, c='k', label='data')
-plt.plot(x, y_rbf, c='g', label='RBF model')
-plt.plot(x, y_lin, c='r', label='Linear model', linestyle='--')
-plt.plot(x, y_poly, c='b', label='Polynomial model',linestyle=':')
+plt.scatter(x, y, c='k', label='data', marker = '.')
+plt.plot(x, y_rbf, c='r', label='RBF model', lw = 3)
+plt.plot(x, np.sin(x), c='g', lw = 3 , linestyle='--')
 plt.xlabel('data')
 plt.ylabel('target')
 plt.title('Support Vector Regression')
